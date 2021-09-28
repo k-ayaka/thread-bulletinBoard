c1ef525cbbe4e96842910b7ab83c3adcba162d26
k-ayaka <o95.kitamura.ayaka@gmail.com>
2021-09-28 16:54:53 +0900

    入力確認画面/登録処理_実装

---
+++ php/models/thread_confirm.php
@@ -0,0 +1,54 @@
+<?php
+
+  session_start();
+  require_once './common/database.php';
+  require_once './common/auth.php';
+
+  if(!isLogin()) {
+    header('Location: top.php');
+    exit;
+  }
+
+  // セッションの内容が含まれているか判定
+  if(!isset($_SESSION['thread_confirm'])) {
+  // セッションの内容が正しくなければ、新規スレッド作成画面へ遷移
+  header('Location: thread_regist.php');
+  exit;
+  }
+
+  // POSTされたデータを変数に代入
+  $title = $_SESSION['thread_confirm']['title'];
+  $content = $_SESSION['thread_confirm']['content'];
+
+  // ログインしているユーザーのIdを取得
+  $member_id = getUserId();
+
+  // 「スレッドを作成する」ボタンを押すと以下のクエリ文が発行
+    if(!empty($_POST)) {
+        //DB接続
+        $database_handler = getDatabaseConnection();
+        
+        try {
+            if($statement = $database_handler->prepare("INSERT INTO threads (member_id, title, content) VALUES(:member_id, :title, :content)")) {
+
+                // bindParamでそれぞれのカラムに、取得した値をバインドする。
+                $statement->bindParam(':member_id', $member_id);
+                $statement->bindParam(':title', htmlspecialchars($title));
+                $statement->bindParam(':content', htmlspecialchars($content));
+                $statement->execute();
+
+                // セッションデータを削除
+                unset($_SESSION['thread_confirm']);
+
+                // 投稿ができたらトップ画面へ遷移
+                header('Location: top.php');
+                exit;
+            }
+
+        } catch(Throwable $e) {
+            echo $e->getMessage();
+            exit;
+        }
+    }
+
+?>
